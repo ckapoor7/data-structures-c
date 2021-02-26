783cb1df72dd55ffb5f14f516d71126763ffc6f2
Chaitanya Kapoor <49002516+ckapoor7@users.noreply.github.com>
2021-02-26 09:56:16 +0530

    Update queue-special.h

--- queues/queue-special.h
+++ queues/queue-special.h
@@ -9,8 +9,8 @@ struct queue{
   int *arr;
 };
 
-void add(int, struct queue*);//Add integer elements to the queue
-int delete(struct queue*); //Delete an element from the queue
+void enqueue(int, struct queue*);//Add integer elements to the queue
+int dequeue(struct queue*); //Delete an element from the queue
 struct queue* init(int); //Initialize a queue of a given size
 bool full(struct queue*); //Check for a full queue
 bool empty(struct queue*); //Check for an empty queue
@@ -18,3 +18,4 @@ void print(struct queue*); //Print a queue
 
 
 #endif
+
